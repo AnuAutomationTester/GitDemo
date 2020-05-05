b5dd85eca0c405a7e7f2b9ecf956f325786d1a65
Bestizer <expertautomationtester@gmail.com>
2020-05-05 14:46:23 +0530

    adding new DBTest3 file

---
+++ DatabaseTesting/src/DBTest3.java
@@ -0,0 +1,15 @@
+
+public class DBTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello this is my git testing file");
+System.out.println("This project descibes the MySql and selenium connection and the crentials are taked from the database to login into the Salesforce url");
+System.out.println("Hope you are enjoying the course");
+System.out.println("This project descibes the MySql and selenium connection and the crentials are taked from the database to login into the Salesforce url");
+System.out.println("Hope you are enjoying the course");
+System.out.println("Hope you are enjoying the course");
+
+	}
+
+}
