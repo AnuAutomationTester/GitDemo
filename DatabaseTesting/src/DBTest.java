28a64440c90d7be10949c4e7d3989982f8c62707
Bestizer <expertautomationtester@gmail.com>
2020-05-05 13:38:58 +0530

    adding DBTestfile

---
+++ DatabaseTesting/src/DBTest.java
@@ -0,0 +1,11 @@
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello this is my git testing file");
+System.out.println("This project descibes the MySql and selenium connection and the crentials are taked from the database to login into the Salesforce url");
+
+	}
+
+}
